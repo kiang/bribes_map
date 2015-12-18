a8287818a3cebeb20c75cfba83112ec4bd779c85
kiang <kiange@gmail.com>
2015-12-18 14:33:32 +0800

    generate points.json

--- bin/jaystar0423.php
+++ bin/jaystar0423.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * data from https://www.ptt.cc/bbs/Gossiping/M.1450410800.A.F6D.html
  */
@@ -13,9 +14,39 @@ $items = array(
     '彰化' => 'zy_xY-hQlid4.kerVOcjUyb_o',
 );
 
+$result = array();
+
 foreach ($items AS $area => $item) {
     $rawFile = $rawPath . '/' . $item . '.xml';
     if (!file_exists($rawFile)) {
         file_put_contents($rawFile, file_get_contents('https://mapsengine.google.com/map/u/0/kml?forcekml=1&mid=' . $item));
     }
+    $xml = simplexml_load_file($rawFile, null, LIBXML_NOCDATA);
+    foreach ($xml->Document->Folder AS $folder) {
+        foreach ($folder->Placemark AS $placemark) {
+            $name = (string) $placemark->name;
+            if (substr($name, 0, 1) === '(') {
+                $pos = strpos($name, ')');
+                $locationText = substr($name, 1, $pos - 1);
+                $title = substr($name, $pos + 1);
+                $year = null;
+                $yearPos = strpos($title, '年');
+                if (false !== $yearPos) {
+                    $year = substr($title, 0, $yearPos);
+                    $title = substr($title, $yearPos + 3);
+                }
+                $coordinates = explode(',', (string) $placemark->Point->coordinates);
+                $result[] = array(
+                    'area' => $area,
+                    'location' => $locationText,
+                    'year' => $year,
+                    'title' => $title,
+                    'description' => (string) $placemark->description,
+                    'latitude' => $coordinates[1],
+                    'longitude' => $coordinates[0],
+                );
+            }
+        }
+    }
 }
+file_put_contents(dirname(__DIR__) . '/points.json', json_encode($result, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
