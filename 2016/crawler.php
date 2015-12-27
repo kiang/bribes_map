64f63cb64a627e51d8ff5209689c73b9b2a8fc52
kiang <kiange@gmail.com>
2015-12-27 15:08:02 +0800

    let crawler search by keyword

--- 2016/crawler.php
+++ 2016/crawler.php
@@ -6,12 +6,51 @@
 date_default_timezone_set('Asia/Taipei');
 $courts = array(
     /*
-    'TPC' => '司法院－刑事補償',
-    'TPU' => '司法院－訴願決定',
-    'TPJ' => '司法院職務法庭',
-    'TPA' => '最高行政法院',
-    'TPP' => '公務員懲戒委員會',
-    'IPC' => '智慧財產法院',
+      'TPC' => '司法院－刑事補償',
+      'TPU' => '司法院－訴願決定',
+      'TPJ' => '司法院職務法庭',
+      'TPA' => '最高行政法院',
+      'TPP' => '公務員懲戒委員會',
+      'IPC' => '智慧財產法院',
+      'PTE' => '屏東簡易庭',
+      'KSY' => '臺灣高雄少年及家事法院',
+      'TPE' => '臺北簡易庭',
+      'STE' => '新店簡易庭',
+      'SLE' => '士林簡易庭',
+      'NHE' => '內湖簡易庭',
+      'PCE' => '板橋簡易庭',
+      'SJE' => '三重簡易庭',
+      'TYE' => '桃園簡易庭',
+      'CLE' => '中壢簡易庭',
+      'SCD' => '新竹簡易庭',
+      'CPE' => '竹北簡易庭(含竹東)',
+      'MLD' => '苗栗簡易庭',
+      'TCE' => '臺中簡易庭',
+      'SDE' => '沙鹿簡易庭',
+      'FYE' => '豐原簡易庭',
+      'CHE' => '彰化簡易庭',
+      'OLE' => '員林簡易庭',
+      'PDE' => '北斗簡易庭',
+      'NTE' => '南投簡易庭(含埔里)',
+      'TLE' => '斗六簡易庭',
+      'HUE' => '虎尾簡易庭',
+      'CYE' => '嘉義簡易庭(含朴子)',
+      'PKE' => '北港簡易庭',
+      'TNE' => '臺南簡易庭',
+      'SYE' => '柳營簡易庭',
+      'SSE' => '新市簡易庭',
+      'KSE' => '高雄簡易庭',
+      'GSE' => '岡山簡易庭',
+      'CSE' => '旗山簡易庭',
+      'FSE' => '鳳山簡易庭',
+      'CCE' => '潮州簡易庭',
+      'TTE' => '臺東簡易庭',
+      'HLE' => '花蓮簡易庭(含鳳林,玉里)',
+      'ILE' => '宜蘭簡易庭',
+      'LTE' => '羅東簡易庭',
+      'KLD' => '基隆、瑞芳簡易庭',
+      'MKE' => '馬公簡易庭',
+      'KME' => '金城簡易庭',
      * 
      */
     'TPS' => '最高法院',
@@ -42,119 +81,94 @@ $courts = array(
     'PHD' => '臺灣澎湖地方法院',
     'KMD' => '福建金門地方法院',
     'LCD' => '福建連江地方法院',
-    /*
-    'PTE' => '屏東簡易庭',
-    'KSY' => '臺灣高雄少年及家事法院',
-    'TPE' => '臺北簡易庭',
-    'STE' => '新店簡易庭',
-    'SLE' => '士林簡易庭',
-    'NHE' => '內湖簡易庭',
-    'PCE' => '板橋簡易庭',
-    'SJE' => '三重簡易庭',
-    'TYE' => '桃園簡易庭',
-    'CLE' => '中壢簡易庭',
-    'SCD' => '新竹簡易庭',
-    'CPE' => '竹北簡易庭(含竹東)',
-    'MLD' => '苗栗簡易庭',
-    'TCE' => '臺中簡易庭',
-    'SDE' => '沙鹿簡易庭',
-    'FYE' => '豐原簡易庭',
-    'CHE' => '彰化簡易庭',
-    'OLE' => '員林簡易庭',
-    'PDE' => '北斗簡易庭',
-    'NTE' => '南投簡易庭(含埔里)',
-    'TLE' => '斗六簡易庭',
-    'HUE' => '虎尾簡易庭',
-    'CYE' => '嘉義簡易庭(含朴子)',
-    'PKE' => '北港簡易庭',
-    'TNE' => '臺南簡易庭',
-    'SYE' => '柳營簡易庭',
-    'SSE' => '新市簡易庭',
-    'KSE' => '高雄簡易庭',
-    'GSE' => '岡山簡易庭',
-    'CSE' => '旗山簡易庭',
-    'FSE' => '鳳山簡易庭',
-    'CCE' => '潮州簡易庭',
-    'TTE' => '臺東簡易庭',
-    'HLE' => '花蓮簡易庭(含鳳林,玉里)',
-    'ILE' => '宜蘭簡易庭',
-    'LTE' => '羅東簡易庭',
-    'KLD' => '基隆、瑞芳簡易庭',
-    'MKE' => '馬公簡易庭',
-    'KME' => '金城簡易庭',
-     * 
-     */
 );
 
-$keyword = '野生動物保育法';
+$tmpPath = dirname(__DIR__) . '/tmp/2016';
+if (!file_exists($tmpPath)) {
+    mkdir($tmpPath, 0777);
+}
 
-foreach ($courts as $court_id => $court) {
-    $url = "http://jirs.judicial.gov.tw/FJUD/FJUDQRY02_1.aspx";
-    if ($court_id[2] == 'E') {
-        $param = "sel_v_court={$court_id}+" . urlencode($court) . "&v_court={$court_id}+" . urlencode($court) . "&v_sys=M&jud_year=&jud_case=&jud_no=&jud_title=" . urlencode($keyword) . "&jt=" . urlencode($keyword) . "&keyword=&sdate=&edate=&page=&searchkw=&sel_judword=" . urlencode('常用字別');
-    } else {
-        $param = "v_court={$court_id}+" . urlencode($court) . "&v_sys=M&jud_year=&jud_case=&jud_no=&jud_title=" . urlencode($keyword) . "&jt=" . urlencode($keyword) . "&keyword=&sdate=&edate=&page=&searchkw=";
+$fh = fopen(__DIR__ . '/list.csv', 'r');
+fgetcsv($fh, 2048);
+while ($line = fgetcsv($fh, 2048)) {
+    $tokenFile = $tmpPath . '/' . $line[0];
+    if (file_exists($tokenFile)) {
+        continue;
     }
-    error_log(urldecode($url . '?' . $param));
+    $keyword = $line[0];
+    foreach ($courts as $court_id => $court) {
+        $url = "http://jirs.judicial.gov.tw/FJUD/FJUDQRY02_1.aspx";
+        if ($court_id[2] == 'E') {
+            $param = "sel_v_court={$court_id}+" . urlencode($court) . "&v_court={$court_id}+" . urlencode($court) . "&v_sys=M&jud_year=&jud_case=&jud_no=&jud_title=&jt=&keyword=" . urlencode($keyword) . "&sdate=&edate=&page=&searchkw=&sel_judword=" . urlencode('常用字別');
+        } else {
+            $param = "v_court={$court_id}+" . urlencode($court) . "&v_sys=M&jud_year=&jud_case=&jud_no=&jud_title=&jt=&keyword=" . urlencode($keyword) . "&sdate=&edate=&page=&searchkw=";
+        }
+        error_log(urldecode($url . '?' . $param));
 
-    $curl = curl_init($url);
-    curl_setopt($curl, CURLOPT_REFERER, $url);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, $param);
-    curl_setopt($curl, CURLOPT_COOKIESESSION, true);
-    curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36');
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    $content = curl_exec($curl);
-    //curl_close($curl);
-    if (!preg_match('#(\d+)\s+筆 / 每頁\s+20\s+筆 / 共\s+\d+\s+頁 / 現在第#m', $content, $matches)) {
-        //print_r($content);
-        continue;
-        throw new Exception('test');
-    }
-    $count = $matches[1];
-    if (!preg_match('#FJUDQRY03_1\.aspx\?id=[0-9]*&([^"]*)#', $content, $matches)) {
-        continue;
-        var_dump($content);
-        throw new Exception('test2');
-    }
-    $param = $matches[1];
-    for ($j = 1; $j <= $count; $j ++) {
-        $case_url = "http://jirs.judicial.gov.tw/FJUD/FJUDQRY03_1.aspx";
-        error_log("{$j}/{$count} {$case_url}");
-        curl_setopt($curl, CURLOPT_POSTFIELDS, "id={$j}&{$param}");
-        curl_setopt($curl, CURLOPT_URL, $case_url);
-        curl_setopt($curl, CURLOPT_REFERER, 'http://jirs.judicial.gov.tw/FJUD/FJUDQRY02_1.aspx');
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_REFERER, $url);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $param);
+        curl_setopt($curl, CURLOPT_COOKIESESSION, true);
         curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36');
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        sleep(1);
         $content = curl_exec($curl);
-
-        if (!preg_match('#href="([^"]*)">友善列印#', $content, $matches)) {
-            error_log($content);
-            throw new Exception('test3');
+        //curl_close($curl);
+        if (!preg_match('#(\d+)\s+筆 / 每頁\s+20\s+筆 / 共\s+\d+\s+頁 / 現在第#m', $content, $matches)) {
+            //print_r($content);
+            continue;
+            throw new Exception('test');
         }
-        $print_url = $matches[1];
-        $query = parse_url($print_url, PHP_URL_QUERY);
-        parse_str($query, $ret);
-        /*
-          ["jrecno"]=>
-          string(26) "104,司促,2243,20150130,1"
-          ["v_court"]=>
-          string(28) "TPD 臺灣臺北地方法院"
-          ["v_sys"]=>
-          string(1) "V"
-          ["jyear"]=>
-          string(3) "104"
-          ["jcase"]=>
-          string(6) "司促"
-          ["jno"]=>
-          string(4) "2243"
-          ["jdate"]=>
-          string(7) "1040130"
-          ["jcheck"]=>
-          string(1) "1"
-         */
-        $court = explode(' ', $ret['v_court'])[0];
-        file_put_contents("outputs/{$court}-{$ret['v_sys']}-{$ret['jyear']}-{$ret['jcase']}-{$ret['jno']}-{$ret['jcheck']}.txt", $content);
-        echo "http://judicial.ronny.tw/{$court}/{$ret['v_sys']}/{$ret['jyear']}/" . urlencode($ret['jcase']) . "/{$ret['jno']}\n";
+        $count = $matches[1];
+        if (!preg_match('#FJUDQRY03_1\.aspx\?id=[0-9]*&([^"]*)#', $content, $matches)) {
+            continue;
+            var_dump($content);
+            throw new Exception('test2');
+        }
+        $param = $matches[1];
+        for ($j = 1; $j <= $count; $j ++) {
+            $case_url = "http://jirs.judicial.gov.tw/FJUD/FJUDQRY03_1.aspx";
+            error_log("{$j}/{$count} {$case_url}");
+            curl_setopt($curl, CURLOPT_POSTFIELDS, "id={$j}&{$param}");
+            curl_setopt($curl, CURLOPT_URL, $case_url);
+            curl_setopt($curl, CURLOPT_REFERER, 'http://jirs.judicial.gov.tw/FJUD/FJUDQRY02_1.aspx');
+            curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36');
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            sleep(1);
+            $content = curl_exec($curl);
+
+            if (!preg_match('#href="([^"]*)">友善列印#', $content, $matches)) {
+                error_log($content);
+                throw new Exception('test3');
+            }
+            $print_url = $matches[1];
+            $query = parse_url($print_url, PHP_URL_QUERY);
+            parse_str($query, $ret);
+            /*
+              ["jrecno"]=>
+              string(26) "104,司促,2243,20150130,1"
+              ["v_court"]=>
+              string(28) "TPD 臺灣臺北地方法院"
+              ["v_sys"]=>
+              string(1) "V"
+              ["jyear"]=>
+              string(3) "104"
+              ["jcase"]=>
+              string(6) "司促"
+              ["jno"]=>
+              string(4) "2243"
+              ["jdate"]=>
+              string(7) "1040130"
+              ["jcheck"]=>
+              string(1) "1"
+             */
+            $court = explode(' ', $ret['v_court'])[0];
+            $path = __DIR__ . "/case/{$line[0]}";
+            if (!file_exists($path)) {
+                mkdir($path, 0777, true);
+            }
+            file_put_contents($path . "/{$court}-{$ret['v_sys']}-{$ret['jyear']}-{$ret['jcase']}-{$ret['jno']}-{$ret['jcheck']}.txt", $content);
+            echo "http://judicial.ronny.tw/{$court}/{$ret['v_sys']}/{$ret['jyear']}/" . urlencode($ret['jcase']) . "/{$ret['jno']}\n";
+        }
     }
+    file_put_contents($tmpPath . '/' . $line[0], '1');
 }
